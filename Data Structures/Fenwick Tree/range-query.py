a9703b8bb93b831f68e9b348d101b8e9b9e0df83
sujin <lksujins@gmail.com>
2021-07-22 08:46:21 +0530

    Fenwick Tree - Point Update and Query

--- Data Structures/Fenwick Tree/range-query.py
+++ Data Structures/Fenwick Tree/range-query.py
@@ -1,11 +1,35 @@
-# Implementation of Fenwick Tree - Range Query
+# Implementation of Fenwick Tree - Range Query (Binary Indexed Tree)
 
-def getSum(BITree: list, index: int) -> int:
-    summ = 0 
-    index = index + 1
-    while index > 0:
-        summ += BITree[index] 
-        index -= index & (-index)
-    return summ
+class BIT:
+    def __init__(self, arr):
+        self.bITree = [0] * (len(arr)+1)
+        for i, val in enumerate(arr):
+            self.update(i, val)
 
-print(getSum([0,1,2,3,4,5,6], 2))
+    def getSum(self, index):
+        summ = 0 
+        index = index + 1
+        while index > 0:
+            summ += self.bITree[index]
+            
+            # TO GET THE PARENT 
+            # Take 2's complement. AND to the orginal value. Subtract from the original value
+            index -= self.lsb(index)
+        return summ
+
+    def lsb(self, val):
+        return val & (-val)        
+        
+    def update(self, index, val):
+        index+=1
+        while index < len(self.bITree):
+            self.bITree[index] += val
+
+            # TO GET THE NEXT INDEX TO UPDATE
+            # Take 2's complement. AND to the orginal value. Add the original value
+            index += self.lsb(index)
+
+
+# Driver code
+bit = BIT([1,5,1,1,10])
+print(bit.getSum(2))
