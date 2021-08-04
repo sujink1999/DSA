3a69719348b723f6b5125770919a3e59361dbb13
Sujin K <lksujins@gmail.com>
2021-08-04 13:19:18 +0530

    Leetcode - Path Sum II

--- Problems and Solutions/path-sum-2.py
+++ Problems and Solutions/path-sum-2.py
@@ -5,6 +5,7 @@
 # Input: root = [5,4,8,11,null,13,4,7,2,null,null,5,1], targetSum = 22
 # Output: [[5,4,11,2],[5,8,4,5]]
 
+
 class Solution:
     def pathSum(self, root: TreeNode, targetSum: int) -> List[List[int]]:
         ans = []
